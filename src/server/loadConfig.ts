83b75643764f963d3eafa6a1b0e81d6a0f027f02
Sonny Johannesson <sonny.johannesson@gmail.com>
2017-11-28 22:23:02 +0100

    Config is now managed using nconf and allows argv, env, and multiple files to be used

--- src/server/loadConfig.ts
+++ src/server/loadConfig.ts
@@ -2,8 +2,31 @@ import { readJSONSync } from "fs-extra";
 import * as path from "path";
 import * as process from "process";
 import { IConfig } from "./IConfig";
+import * as nconf from "nconf";
 
 export function loadConfig(): IConfig {
-    const configPath = path.join(process.cwd(), "config.json");
-    return readJSONSync(configPath);
+    nconf
+        .argv()
+        .env({ lowerCase: true, separator: "_" })
+        .file("/var/config/staticless.json")
+        .file("workingDirectory", path.join(process.cwd(), "staticless.json"))
+        .file("appDirectory", path.join(__dirname, "staticless.json"))
+        .required([
+            "frontend:title",
+            "gitlab:url",
+            "gitlab:apiToken",
+            "gitlab:projectId"
+        ]);
+
+    return {
+        frontend: {
+            title: nconf.get("frontend:title")
+        },
+
+        gitlab: {
+            apiToken: nconf.get("gitlab:apiToken"),
+            projectId: nconf.get("gitlab:projectId"),
+            url: nconf.get("gitlab:url")
+        }
+    };
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,73 @@
 const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-    entry: "./src/client/app.tsx",
+module.exports = (env) => {
+    const clientFiles = [
+        { from: "./src/client/index.html" },
+        { from: "./src/client/app.css" },
+        { from: "./src/client/favicon.ico" },
+        { from: "./node_modules/highlight.js/styles/atom-one-dark.css", to: "code-block-styles/atom-one-dark.css" },
+        { from: "./node_modules/highlight.js/styles/atom-one-light.css", to: "code-block-styles/atom-one-light.css" },
+        { from: "./node_modules/highlight.js/styles/monokai.css", to: "code-block-styles/monokai.css" },
+        { from: "./node_modules/highlight.js/styles/solarized-dark.css", to: "code-block-styles/solarized-dark.css" },
+        { from: "./node_modules/highlight.js/styles/solarized-light.css", to: "code-block-styles/solarized-light.css" },
+        { from: "./node_modules/highlight.js/styles/tomorrow.css", to: "code-block-styles/tomorrow.css" }
+    ];
 
-    output: {
-        filename: "app.js",
-        path: path.resolve(__dirname, "dist/client")
-    },
+    if (env.prod === 'false') {
+        clientFiles.push({ from: "./staticless.json", to: "../staticless.json" });
+    }
 
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".jsx"]
-    },
+    return {
+        entry: "./src/client/app.tsx",
 
-    devtool: "inline-source-map",
+        output: {
+            filename: "app.js",
+            path: path.resolve(__dirname, "dist/client")
+        },
 
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: [
-                    {
-                        loader: "awesome-typescript-loader",
-                        options: {
-                            configFileName: "./src/client/tsconfig.json"
+        resolve: {
+            extensions: [".ts", ".tsx", ".js", ".jsx"]
+        },
+
+        devtool: "inline-source-map",
+
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    use: [
+                        {
+                            loader: "awesome-typescript-loader",
+                            options: {
+                                configFileName: "./src/client/tsconfig.json"
+                            }
                         }
-                    }
-                ],
-                exclude: /node_modules/
-            },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [
-                    { loader: "style-loader" },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: true,
-                            namedExport: true,
-                            importLoaders: 2,
-                            localIdentName: "[local]__[hash:base64:5]"
-                        }
-                    },
-                    { loader: "postcss-loader" }
-                ]
-            }
+                    ],
+                    exclude: /node_modules/
+                },
+                {
+                    test: /\.css$/,
+                    exclude: /node_modules/,
+                    use: [
+                        { loader: "style-loader" },
+                        {
+                            loader: "css-loader",
+                            options: {
+                                modules: true,
+                                namedExport: true,
+                                importLoaders: 2,
+                                localIdentName: "[local]__[hash:base64:5]"
+                            }
+                        },
+                        { loader: "postcss-loader" }
+                    ]
+                }
+            ]
+        },
+
+        plugins: [
+            new CopyWebpackPlugin(clientFiles)
         ]
-    },
-
-    plugins: [
-        new CopyWebpackPlugin([
-            { from: "./src/client/index.html" },
-            { from: "./src/client/app.css" },
-            { from: "./src/client/favicon.ico" },
-            { from: "./config.json", to: "../config.json" },
-            { from: "./node_modules/highlight.js/styles/atom-one-dark.css", to: "code-block-styles/atom-one-dark.css" },
-            { from: "./node_modules/highlight.js/styles/atom-one-light.css", to: "code-block-styles/atom-one-light.css" },
-            { from: "./node_modules/highlight.js/styles/monokai.css", to: "code-block-styles/monokai.css" },
-            { from: "./node_modules/highlight.js/styles/solarized-dark.css", to: "code-block-styles/solarized-dark.css" },
-            { from: "./node_modules/highlight.js/styles/solarized-light.css", to: "code-block-styles/solarized-light.css" },
-            { from: "./node_modules/highlight.js/styles/tomorrow.css", to: "code-block-styles/tomorrow.css" }
-        ])
-    ]
+    }
 }
